6e30c12fb5487c560005a6c0446bab922b7b61c5
Oliver Soeser <o.soeser@icloud.com>
2024-07-11 13:36:45 +0100

    Initial commit

---
+++ build/index.js
@@ -0,0 +1,144 @@
+var Vector = (function () {
+    function Vector(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    Vector.prototype.xval = function () { return this.x; };
+    Vector.prototype.yval = function () { return this.y; };
+    Vector.prototype.add = function (other) {
+        return new Vector(this.x + other.x, this.y + other.y);
+    };
+    Vector.prototype.sub = function (other) {
+        return new Vector(this.x - other.x, this.y - other.y);
+    };
+    Vector.prototype.mul = function (other) {
+        return new Vector(other * this.x, other * this.y);
+    };
+    Vector.prototype.size = function () {
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    };
+    return Vector;
+}());
+var Mouse = (function () {
+    function Mouse() {
+        this.down = false;
+        this.clickpos = ZERO_VEC;
+        this.releasepos = ZERO_VEC;
+        this.pos = ZERO_VEC;
+    }
+    return Mouse;
+}());
+var Particle = (function () {
+    function Particle(pos, vel, mass) {
+        this.current_force = ZERO_VEC;
+        this.pos = pos;
+        this.vel = vel;
+        this.mass = mass;
+    }
+    Particle.prototype.step = function () {
+        var _this = this;
+        pobject_array.forEach(function (pobject) {
+            _this.applyForce(_this.gravitationalForce(pobject));
+        });
+        var acc = this.current_force.mul(1 / this.mass);
+        this.vel = this.vel.add(acc.mul(dt));
+        this.pos = this.pos.add(this.vel.mul(dt));
+        this.current_force = ZERO_VEC;
+    };
+    Particle.prototype.posval = function () { return this.pos; };
+    Particle.prototype.massval = function () { return this.mass; };
+    Particle.prototype.applyForce = function (force) {
+        this.current_force = this.current_force.add(force);
+    };
+    Particle.prototype.gravitationalForce = function (other) {
+        var r_vec = other.posval().sub(this.pos);
+        var r = r_vec.size();
+        return r_vec.mul(G * this.mass * other.massval() / (Math.pow(r, 3) + 1));
+    };
+    return Particle;
+}());
+var Renderer = (function () {
+    function Renderer(element_id) {
+        this.bg_color = "#0F0F0F";
+        this.canvas = document.getElementById(element_id);
+        this.context = this.canvas.getContext("2d");
+        this.fitCanvasToWindow();
+        this.drawBackground();
+        setInterval(this.step.bind(this), SECOND / FPS);
+    }
+    Renderer.prototype.addListener = function (event, listener) {
+        this.canvas.addEventListener(event, listener);
+    };
+    Renderer.prototype.step = function () {
+        var _this = this;
+        this.fitCanvasToWindow();
+        this.drawBackground();
+        pobject_array.forEach(function (pobject) {
+            pobject.step();
+            _this.drawPObject(pobject);
+        });
+        if (mouse.down) {
+            this.drawPObject(new Particle(mouse.clickpos, ZERO_VEC, Date.now() - mouse.clicktime));
+        }
+    };
+    Renderer.prototype.fitCanvasToWindow = function () {
+        this.canvas.width = window.innerWidth + 1;
+        this.canvas.height = window.innerHeight + 1;
+    };
+    Renderer.prototype.drawBackground = function () {
+        this.context.fillStyle = this.bg_color;
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    };
+    Renderer.prototype.drawPObject = function (pobject) {
+        var pos = pobject.posval();
+        var mass = pobject.massval();
+        this.context.strokeStyle = "#ffffff";
+        this.context.beginPath();
+        this.context.arc(pos.xval(), pos.yval(), Math.sqrt(mass), 0, 2 * PI);
+        this.context.stroke();
+    };
+    return Renderer;
+}());
+var Physics = (function () {
+    function Physics() {
+        this.renderer = new Renderer("canvas");
+    }
+    Physics.prototype.createParticle = function (pos, vel, mass) {
+        pobject_array.push(new Particle(pos, vel, mass));
+    };
+    return Physics;
+}());
+var UserInput = (function () {
+    function UserInput() {
+        var _this = this;
+        this.physics = new Physics();
+        mouse = new Mouse();
+        this.physics.renderer.addListener("mousedown", function (event) {
+            mouse.down = true;
+            mouse.clickpos = new Vector(event.x, event.y);
+            mouse.clicktime = Date.now();
+        });
+        this.physics.renderer.addListener("mousemove", function (event) {
+            mouse.pos = new Vector(event.x, event.y);
+        });
+        this.physics.renderer.addListener("mouseup", function (event) {
+            mouse.down = false;
+            mouse.releasepos = new Vector(event.x, event.y);
+            mouse.releasetime = Date.now();
+            var vel = mouse.releasepos.sub(mouse.clickpos).mul(1 / 10);
+            _this.physics.createParticle(mouse.clickpos, vel, mouse.releasetime - mouse.clicktime);
+        });
+    }
+    return UserInput;
+}());
+var SECOND = 1000;
+var FPS = 60;
+var dt = 1 / FPS;
+var G = 10;
+var PI = Math.PI;
+var ZERO_VEC = new Vector(0, 0);
+var pobject_array = new Array();
+var mouse = new Mouse();
+window.onload = function () {
+    new UserInput();
+};
---
+++ src/index.ts
@@ -0,0 +1,236 @@
+// Vectors
+class Vector {
+    private x: number;
+    private y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Getters
+    xval() { return this.x; }
+    yval() { return this.y; }
+
+    // Vector Addition
+    add(other: Vector) {
+        return new Vector(
+            this.x + other.x,
+            this.y + other.y
+        );
+    }
+
+    // Vector Subtraction
+    sub(other: Vector) {
+        return new Vector(
+            this.x - other.x,
+            this.y - other.y
+        );
+    }
+
+    // Scalar Multiplication
+    mul(other: number): Vector {
+        return new Vector(
+            other * this.x,
+            other * this.y
+        );
+    }
+
+    // Vector Magnitude
+    size(): number {
+        return Math.sqrt(this.x**2 + this.y**2);
+    }
+}
+
+// Mouse
+class Mouse {
+    public down: boolean;
+    public clickpos: Vector
+    public clicktime: number;
+    public releasepos: Vector
+    public releasetime: number;
+    public pos: Vector;
+
+    constructor() {
+        this.down = false;
+        this.clickpos = ZERO_VEC;
+        this.releasepos = ZERO_VEC;
+        this.pos = ZERO_VEC;
+    }
+}
+
+// Physical Objects
+interface PObject {
+    posval(): Vector;
+    massval(): number;
+    step(): void;
+}
+
+// Particles
+class Particle implements PObject {
+    private pos: Vector;
+    private vel: Vector;
+    private mass: number;
+
+    private current_force: Vector = ZERO_VEC;
+
+    constructor(pos: Vector, vel: Vector, mass: number) {
+        this.pos = pos;
+        this.vel = vel;
+        this.mass = mass;
+    }
+
+    step() {
+        // Sum up gravitational forces
+        pobject_array.forEach(pobject => {
+            this.applyForce(this.gravitationalForce(pobject))
+        });
+
+        // Update velocity and position
+        let acc = this.current_force.mul(1/this.mass);
+        this.vel = this.vel.add(acc.mul(dt));
+        this.pos = this.pos.add(this.vel.mul(dt));
+
+        this.current_force = ZERO_VEC;
+    }
+
+    // Getters
+    posval(): Vector { return this.pos; }
+    massval(): number { return this.mass; }
+
+    // Add force to total for the current step
+    applyForce(force: Vector) {
+        this.current_force = this.current_force.add(force);
+    }
+
+    // Calculate gravity between two objects
+    gravitationalForce(other: PObject): Vector {
+        let r_vec = other.posval().sub(this.pos);
+        let r = r_vec.size();
+        return r_vec.mul(G*this.mass*other.massval()/(r**3+1));
+    }
+}
+
+class Renderer {
+    private canvas: HTMLCanvasElement;
+    private context: CanvasRenderingContext2D;
+
+    private bg_color: string = "#0F0F0F";
+
+    constructor(element_id: string) {
+        this.canvas = document.getElementById(element_id) as HTMLCanvasElement;
+        this.context = this.canvas.getContext("2d");
+
+        // Resize window and draw background
+        this.fitCanvasToWindow();
+        this.drawBackground();
+
+        // Set frame update function
+        setInterval(this.step.bind(this), SECOND/FPS);
+    }
+
+    // Event listeners in 
+    addListener(event: string, listener: (e: MouseEvent) => void) {
+        this.canvas.addEventListener(event, listener);
+    }
+
+    step() {
+        // Resize window and draw background
+        this.fitCanvasToWindow()
+        this.drawBackground();
+
+        // Update and draw PObjects
+        pobject_array.forEach(pobject => {
+            pobject.step();
+            this.drawPObject(pobject);
+        });
+
+        // Draw Particle being created
+        if (mouse.down) {
+            this.drawPObject(new Particle(mouse.clickpos, ZERO_VEC, Date.now()-mouse.clicktime));
+        }
+    }
+
+    fitCanvasToWindow() {
+        this.canvas.width = window.innerWidth + 1;
+        this.canvas.height = window.innerHeight + 1;
+    }
+
+    drawBackground() {
+        this.context.fillStyle = this.bg_color;
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    drawPObject(pobject: PObject) {
+        let pos = pobject.posval();
+        let mass = pobject.massval();
+
+        this.context.strokeStyle = "#ffffff";
+        
+        this.context.beginPath();
+        this.context.arc(pos.xval(), pos.yval(), Math.sqrt(mass), 0, 2*PI);
+        this.context.stroke();
+    }
+}
+
+class Physics {
+    public renderer: Renderer;
+
+    constructor() {
+        this.renderer = new Renderer("canvas");
+    }
+
+    createParticle(pos: Vector, vel: Vector, mass: number) {
+        pobject_array.push(new Particle(pos, vel, mass));
+    }
+}
+
+class UserInput {
+    private physics: Physics;
+
+    constructor() {
+        this.physics = new Physics();
+        mouse = new Mouse();
+
+        this.physics.renderer.addListener("mousedown", event => {
+            mouse.down = true;
+            mouse.clickpos = new Vector(event.x, event.y);
+            mouse.clicktime = Date.now();
+        });
+    
+        this.physics.renderer.addListener("mousemove", event => {
+            mouse.pos = new Vector(event.x, event.y);
+        });
+    
+        this.physics.renderer.addListener("mouseup", event => {
+            mouse.down = false;
+            mouse.releasepos = new Vector(event.x, event.y);
+            mouse.releasetime = Date.now();
+
+            let vel = mouse.releasepos.sub(mouse.clickpos).mul(1/10);
+
+            this.physics.createParticle(mouse.clickpos, vel, mouse.releasetime-mouse.clicktime);
+        });
+    }
+}
+
+// Time constants
+const SECOND = 1000;
+const FPS = 60;
+
+// Physical constants
+const dt = 1/FPS;
+const G = 10;
+
+// Mathematical constants
+const PI = Math.PI;
+const ZERO_VEC = new Vector(0, 0);
+
+// Global objects
+let pobject_array: PObject[] = new Array();
+let mouse = new Mouse();
+
+// Start app
+window.onload = () => {
+    new UserInput();
+};
